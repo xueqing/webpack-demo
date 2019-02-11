61866b0e2d69ea8f832ecf9450d4ab5e9742b7b0
kiki <hj.chen@bmi-tech.com>
2019-02-11 18:04:27 +0800

    1. 执行 npm install --save lodash，安装依赖库 2. 在 js 脚本中导入 lodash 模块，删除 html 中对 lodash 的引用 3. 执行 npx webpack 会将脚本作为入口起点，输出 main.js 文件，修改 html 文件中引入的脚本文件路径

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import _ from 'lodash';
+
 function component() {
   var element = document.createElement('div');
 
-  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+  // Lodash 现在通过此脚本文件引入
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
